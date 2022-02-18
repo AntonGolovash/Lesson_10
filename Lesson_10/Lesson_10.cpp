57008ce1022893c6dfa0b3ddb38932166c5b4ddb
Golo_er38@student.itstep.org <Golo_er38@student.itstep.org>
2022-02-18 17:33:42 +0200

    1

--- Lesson_10/Lesson_10.cpp
+++ Lesson_10/Lesson_10.cpp
@@ -22,13 +22,24 @@ void WritedegreeToFile(int numberDegree, string path)
 		File::Append(data, path);
 		cout << "Wrote the data:\t" << data << endl;
 	}
-	Log::Append("Success", path);
+	Log::Append("Success", File::GetPath("log.txt"));
 	door.unlock();
 }
 
 int main()
 {
 	cout << "Thread count:\t" << thread::hardware_concurrency() << endl;
+	//cout << File::GetPath("degree2048.txt") << endl;
+	thread th(WritedegreeToFile, 2048, File::GetPath("degree2048.txt"));
+	thread th1(WritedegreeToFile, 1024, File::GetPath("degree1024.txt"));
+	thread th2(WritedegreeToFile, 4096, File::GetPath("degree4096.txt"));
+
+
+	//cout << this_thread::get_id() << endl;// get id current thread
+
+	th.join();
+	th1.join();
+	th2.join();
 
 	system("pause");
 	return 0;
---
+++ SerDes/SerDes.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+class Student
+{
+public:
+	int _id{ 0 };
+	char _firstname[255];
+	char _lastname[255];
+};
+
+int main()
+{
+	const int size = 3;
+	Student studentsToFile[size] =
+	{
+		{1, "studentname1", "studentlastname1"},
+		{2, "studentname2", "studentlastname2"},
+		{3, "studentname3", "studentlastname3"},
+	};
+
+	//int x = 25;
+	//[x]
+	//{
+	//	std::cout << "X = " << x << std::endl;// лямбда выражение
+	//};
+
+	for (auto& student : studentsToFile)
+		std::cout << student._firstname << std::endl;
+
+	//write
+	ofstream fwrite("students.txt", ios_base::binary);
+	fwrite.write(reinterpret_cast<char*>(&studentsToFile), sizeof(studentsToFile));
+	fwrite.close();
+	system("pause");
+
+	return 0;
+}
